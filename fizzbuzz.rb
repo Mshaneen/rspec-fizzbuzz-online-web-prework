10fe76fb8b40c2afc153b42df3ee2f011f1015c7
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-05 05:54:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,5 @@
-require_relative './spec_helper.rb'
-require_relative '../fizzbuzz.rb'
-
-
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-   return "FizzBuzz"
-  end
-
-  if int % 5 == 0
-   return "Buzz"
-  end
-
   if int % 3 == 0
-   return "Fizz"
+    "Fizz"
   end
-
 end
